c71c48b4d979300d94dda729c36e084cb271e7f7
Murakami <murakami.edson@gmail.com>
2025-04-14 15:56:13 -0300

    adicionado releasetime

--- src/main/java/com/zoom/controller/ReleaseTimeLineBean.java
+++ src/main/java/com/zoom/controller/ReleaseTimeLineBean.java
@@ -42,13 +42,15 @@ public class ReleaseTimeLineBean implements Serializable {
     @PostConstruct
     protected void initialize() {
         model = new TimelineModel<>();
+        model.add(TimelineEvent.<String>builder().data("v0.2.0")
+        		.startDate(LocalDate.of(2025, 04, 14)).build());
         model.add(TimelineEvent.<String>builder().data("v0.1.0")
         		.startDate(LocalDate.of(2025, 03, 20)).build());
        
     }
 
     /* LAST VERSION */
-    private String lastVersion = "v0.1.0";
+    private String lastVersion = "v0.2.0";
     /* LAST VERSION */
     
     public void onSelect(TimelineSelectEvent<String> e) {
@@ -56,6 +58,10 @@ public class ReleaseTimeLineBean implements Serializable {
         String versao = timelineEvent.getData();
         
         switch(versao) {
+        case "v0.2.0": 
+	    	MessageUtil.sucesso("14/abr/2025");
+	    	MessageUtil.sucesso("Incluidos Cadastros e Login.");	    	
+	    	break;
         case "v0.1.0": 
 	    	MessageUtil.sucesso("20/mar/2025");
 	    	MessageUtil.sucesso("Início do projeto Zoom.");	    	
